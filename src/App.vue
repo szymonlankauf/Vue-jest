8273a1c555148c83e5956152c9bfbf12888edb21
tesla369 <tesla369@gmail.com>
2019-03-26 15:41:29 +0100

    Vue-jest-test

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="app">
+        <buttonCounter/>
+    </div>
+</template>
+
+<script>
+import buttonCounter from './components/buttonCounter.vue'
+export default {
+  name: 'app',
+  components: {
+      buttonCounter
+  }
+};
+</script>
---
+++ src/components/__tests__/buttonCounter.test.js
@@ -0,0 +1,12 @@
+import { mount } from '@vue/test-utils'
+import buttonCounter from '../buttonCounter.vue'
+
+describe('buttonCounter', () => {
+    it('Increases count when clicked.', () => {
+        const wrapper = mount(buttonCounter);
+        expect(wrapper.vm.count).toBe(0);
+        const button = wrapper.find('button');
+        button.trigger('click');
+        expect(wrapper.vm.count).toBe(1);
+    })
+})
